0920d6dc460f2dd87df8c29db4929d31801800eb
skorohodov97 <71003027+skorohodov97@users.noreply.github.com>
2022-03-30 23:25:47 +0300

    Delete meeting.sh

--- app/api/meeting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-API_KEY="mirotalksfu_default_secret"
-MIROTALK_URL="http://localhost:3010/api/v1/meeting"
-
-curl $MIROTALK_URL \
-    --header "authorization: $API_KEY" \
-    --header "Content-Type: application/json" \
-    --request POST
