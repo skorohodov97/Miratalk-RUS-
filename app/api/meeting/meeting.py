fa8379e129067eac90881d837785ca8f07e4bc15
skorohodov97 <71003027+skorohodov97@users.noreply.github.com>
2022-05-01 20:58:15 +0500

    Add files via upload

--- app/api/meeting/meeting.py
+++ app/api/meeting/meeting.py
@@ -1,10 +1,8 @@
 import requests
 import json
 
-API_KEY = "mirotalk_default_secret"
-# MIROTALK_URL = "http://localhost:3000/api/v1/meeting"
-# MIROTALK_URL = "https://mirotalk.herokuapp.com/api/v1/meeting"
-MIROTALK_URL = "https://mirotalk.up.railway.app/api/v1/meeting"
+API_KEY = "mirotalksfu_default_secret"
+MIROTALK_URL = "https://sfu.mirotalk.org/api/v1/meeting"
 
 headers = {
     "authorization": API_KEY,
--- app/api/meeting/meeting.sh
+++ app/api/meeting/meeting.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-API_KEY="mirotalk_default_secret"
-# MIROTALK_URL="http://localhost:3000/api/v1/meeting"
-# MIROTALK_URL=https://mirotalk.herokuapp.com/api/v1/meeting
-MIROTALK_URL=https://mirotalk.up.railway.app/api/v1/meeting
+API_KEY="mirotalksfu_default_secret"
+MIROTALK_URL="https://sfu.mirotalk.org/api/v1/meeting"
 
 curl $MIROTALK_URL \
     --header "authorization: $API_KEY" \
